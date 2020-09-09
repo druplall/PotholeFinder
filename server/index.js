ffc79a03d45365774802ef4cf3d67a6e11820b70
Deo-Mac <druplall@gmail.com>
2020-09-09 19:35:22 -0400

    test

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
 app.listen(port, function () {
-  console.log(`Listening on port ${port}`);
+  console.log(`Listening on port ${PORT}`);
 });
